76933c0cf15b91b98592d7ffb57ae5a5dcfeddda
soletebarco <soletebarco@192.168.2.1>
2018-11-27 21:28:46 +0100

    hola

--- src/clasesdeemergencia/Sclass.java
+++ src/clasesdeemergencia/Sclass.java
@@ -10,5 +10,28 @@ package clasesdeemergencia;
  * @author dvdsa
  */
 public class Sclass {
-    
+    /*<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        setcookie("Cumpleanios","27/11/2018",time()+2000);
+        if($_COOKIE["Cumpleanios"] === date("d/m/Y")){
+            echo "Feliz cumpleanios";
+        }else{
+            echo "gilipollas";
+        }
+        ?>
+        
+       
+    </body>
+</html>*/
 }
