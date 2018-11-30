9c736462f9cd47ff39d87ef43d38e90a4096736e
User <User@User-PC>
2018-11-30 10:07:55 +0100

    CINE

--- src/clasesdeemergencia/Tclass.java
+++ src/clasesdeemergencia/Tclass.java
@@ -10,6 +10,46 @@ package clasesdeemergencia;
  * @author dvdsa
  */
 public class Tclass {
-    private int n;
-    //que va, no me sale nada, pero lo que se dice nada de nada
+//    HAY QUE MODULARLO, CAMBIAR EL NOMBRE DE LAS VARIABLES EN ESPECIAL LA DE SW.
+//ESO ME HA DICHO, CAMBIADLO. 
+//
+//static Scanner t=new Scanner(System.in);
+//    public static void main(String[] args) {
+//	int casos,vecinos,i,j,d,sw;
+//	int[] butaca= new int[10000];
+//	casos=t.nextInt();
+//	while(casos>0){
+//		i=0;
+//		sw=0;
+//		d=0;
+//		vecinos=t.nextInt();
+//                
+//		while(vecinos>0){
+//                    butaca[i]=t.nextInt();
+//                    if(sw==0&&butaca[i]%2==1)
+//                        sw=i;
+//                    vecinos--;
+//                    i++;
+//		}
+//                
+//		for(j=0;j<i-1;j++)
+//			if(butaca[j]%2==1&&butaca[j+1]%2==0)
+//				d=1;
+//                
+//		if(d==1)
+//                    System.out.println("NO");
+//		else{
+//                    if(d==0&&butaca[0]%2==1)
+//                        System.out.println("SI 0");
+//                    else{
+//			if(d==0&&butaca[i-1]%2==0)
+//                            System.out.println("SI "+i);
+//			else
+//			System.out.println("SI "+sw);
+//                    }
+//		}
+//		casos--;
+//	}
+//    }
+//}
 }
